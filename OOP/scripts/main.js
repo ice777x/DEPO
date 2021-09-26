26fa82738608599ee75714a29792ef6f24ef530a
ice777 <ice777root@gmail.com>
2021-09-26 23:39:46 +0300

    p t s h

--- OOP/scripts/main.js
+++
@@ -1,9 +0,0 @@
-let Person = function (name) {
-	this.name = name;
-};
-Person.prototype.introduce = function () {
-	console.log(`Merhaba ${this.name}`);
-};
-
-let zort = new Person("zort");
-zort.introduce();
