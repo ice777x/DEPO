3be5283d1f6e89deb396526819c7554f6af99e02
ice777x <68118276+ice777x@users.noreply.github.com>
2022-04-17 13:36:36 +0300

    Update iceREQ.py

--- Python/iceREQ.py
+++ Python/iceREQ.py
@@ -5,8 +5,38 @@ import asyncio
 kimlik = {
     "User-Agent": "Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/41.0.2227.0 Safari/537.36"
 }
-linkler = ["https://www.trendyol.com/sr?q=ceket&qt=ceket&st=ceket&os=1"]
-
+linkler = (
+    "https://www.trendyol.com/canavice/6-li-mutfak-havlusu-lavanta-seti-40x60-p-171630661",
+    "https://www.trendyol.com/zeynep-tekstil/damask-4-lu-50x90-cm-el-yuz-havlu-seti-p-87609700",
+    "https://www.trendyol.com/burtay-home/pamuklu-lux-boncuklu-gupurlu-havlu-6-li-50x90-rose-p-237085027",
+    "https://www.trendyol.com/louis-marie/banyo-havlu-takimi-70x140-50x90-vip-kahve-p-52290033",
+    "https://www.trendyol.com/minteks/kanavice-50x90-diva-6-li-havlu-takimi-krem-renk-p-59324344",
+    "https://www.trendyol.com/kardelen/100-pamuklu-boncuklu-gupurlu-6-li-havlu-50x90-p-91250754",
+    "https://www.trendyol.com/canavice/6-li-mutfak-havlusu-lavanta-seti-40x60-p-171630661",
+    "https://www.trendyol.com/zeynep-tekstil/damask-4-lu-50x90-cm-el-yuz-havlu-seti-p-87609700",
+    "https://www.trendyol.com/burtay-home/pamuklu-lux-boncuklu-gupurlu-havlu-6-li-50x90-rose-p-237085027",
+    "https://www.trendyol.com/louis-marie/banyo-havlu-takimi-70x140-50x90-vip-kahve-p-52290033",
+    "https://www.trendyol.com/minteks/kanavice-50x90-diva-6-li-havlu-takimi-krem-renk-p-59324344",
+    "https://www.trendyol.com/kardelen/100-pamuklu-boncuklu-gupurlu-6-li-havlu-50x90-p-91250754",
+    "https://www.trendyol.com/canavice/6-li-mutfak-havlusu-lavanta-seti-40x60-p-171630661",
+    "https://www.trendyol.com/zeynep-tekstil/damask-4-lu-50x90-cm-el-yuz-havlu-seti-p-87609700",
+    "https://www.trendyol.com/burtay-home/pamuklu-lux-boncuklu-gupurlu-havlu-6-li-50x90-rose-p-237085027",
+    "https://www.trendyol.com/louis-marie/banyo-havlu-takimi-70x140-50x90-vip-kahve-p-52290033",
+    "https://www.trendyol.com/minteks/kanavice-50x90-diva-6-li-havlu-takimi-krem-renk-p-59324344",
+    "https://www.trendyol.com/kardelen/100-pamuklu-boncuklu-gupurlu-6-li-havlu-50x90-p-91250754",
+    "https://www.trendyol.com/canavice/6-li-mutfak-havlusu-lavanta-seti-40x60-p-171630661",
+    "https://www.trendyol.com/zeynep-tekstil/damask-4-lu-50x90-cm-el-yuz-havlu-seti-p-87609700",
+    "https://www.trendyol.com/burtay-home/pamuklu-lux-boncuklu-gupurlu-havlu-6-li-50x90-rose-p-237085027",
+    "https://www.trendyol.com/louis-marie/banyo-havlu-takimi-70x140-50x90-vip-kahve-p-52290033",
+    "https://www.trendyol.com/minteks/kanavice-50x90-diva-6-li-havlu-takimi-krem-renk-p-59324344",
+    "https://www.trendyol.com/kardelen/100-pamuklu-boncuklu-gupurlu-6-li-havlu-50x90-p-91250754",
+    "https://www.trendyol.com/canavice/6-li-mutfak-havlusu-lavanta-seti-40x60-p-171630661",
+    "https://www.trendyol.com/zeynep-tekstil/damask-4-lu-50x90-cm-el-yuz-havlu-seti-p-87609700",
+    "https://www.trendyol.com/burtay-home/pamuklu-lux-boncuklu-gupurlu-havlu-6-li-50x90-rose-p-237085027",
+    "https://www.trendyol.com/louis-marie/banyo-havlu-takimi-70x140-50x90-vip-kahve-p-52290033",
+    "https://www.trendyol.com/minteks/kanavice-50x90-diva-6-li-havlu-takimi-krem-renk-p-59324344",
+    "https://www.trendyol.com/kardelen/100-pamuklu-boncuklu-gupurlu-6-li-havlu-50x90-p-91250754"
+)
 
 def timer(func):  # decorator timer
     def wrapper():
